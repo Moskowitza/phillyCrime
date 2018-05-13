8211ca04c1913e268e9d5197dd417b4df74c70b2
moskowitza <moskowitzaaron@mac.com>
2018-05-13 11:38:29 -0400

    config

--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO burgers (name,devoured) VALUES ('Beyond Burger',false);
-INSERT INTO burgers (name,devoured) VALUES ('Gardien Beefless Burger',true);
-INSERT INTO burgers (name,devoured) VALUES ('Boca Burger',false);
-INSERT INTO burgers (name,devoured) VALUES ('Beast Burger',false);
-INSERT INTO burgers (name,devoured) VALUES ('Portabello Burger',false);
-INSERT INTO burgers (name,devoured) VALUES ('Field Roast Burger',false);
