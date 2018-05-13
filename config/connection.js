caac891c461f8faf13327b16619f4708256d1976
moskowitza <moskowitzaaron@mac.com>
2018-05-13 11:13:52 -0400

    change schema to users

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "mysql2018",
-  database: "burger_db"
+  database: "user_db"
 });
 
 // Make connection.
--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,4 @@
-DROP DATABASE IF EXISTS burger_db;
+DROP DATABASE IF EXISTS user_db;
 
-CREATE DATABASE burger_db;
-USE burger_db;
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
-INSERT INTO burgers (name,devoured) VALUES ('Beyond Burger',false);
-INSERT INTO burgers (name,devoured) VALUES ('Gardien Beefless Burger',true);
-INSERT INTO burgers (name,devoured) VALUES ('Boca Burger',false);
-INSERT INTO burgers (name,devoured) VALUES ('Beast Burger',false);
-INSERT INTO burgers (name,devoured) VALUES ('Portabello Burger',true);
-INSERT INTO burgers (name,devoured) VALUES ('Field Roast Burger',false);
+CREATE DATABASE user_db;
+USE user_db;
