b7a743509f4ac199873ccff7c9d6d0918b9ea796
moskowitza <moskowitzaaron@mac.com>
2018-05-17 16:30:38 -0400

    readme

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+var db = require("./app/models");
 
 // Sets up the Express app to handle data parsing
 
@@ -23,20 +23,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static("./app/public"));
 
-// Set Handlebars.
-//=============================================================
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// // Set Handlebars.
+// //=============================================================
+// var exphbs = require("express-handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
 
 // Routes
 // =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/user-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+require("./app/routes/html-routes.js")(app);
+require("./app/routes/user-api-routes.js")(app);
+require("./app/routes/post-api-routes.js")(app);
 
 
 // Syncing our sequelize models and then starting our Express app
